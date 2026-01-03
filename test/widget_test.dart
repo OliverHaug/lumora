3e4e9d5e28c79c1fecbd4fece102bfd6a54454df
OliverHaug <oliver-haugk@web.de>
2026-01-03 22:48:24 +0100

    git commit -m "test: make MyApp bootable in widget tests without GetX bindings"

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xyz/main.dart';
 
 void main() {
   testWidgets('app boots', (tester) async {
-    await tester.pumpWidget(const MyApp(enableBindings: false));
+    await tester.pumpWidget(
+      MyApp(enableBindings: false, testHome: const Scaffold(body: SizedBox())),
+    );
     await tester.pump();
     expect(true, isTrue);
   });
