35d1886c8af71acd58de2d5747b63563aeea1ce1
OliverHaug <oliver-haugk@web.de>
2026-01-03 22:41:32 +0100

    git commit -m "test: make MyApp bootable in widget tests without GetX bindings"

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       initialBinding: enableBindings ? AppBindings() : null,
       getPages: AppPages.routes,
-      initialRoute: '/splash',
+      initialRoute: enableBindings ? '/splash' : '/',
     );
   }
 }
