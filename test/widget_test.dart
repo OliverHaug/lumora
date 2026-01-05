28cb24207d3a1e5a6abc26bc69f1494e627e3854
OliverHaug <oliver-haugk@web.de>
2026-01-05 02:02:14 +0100

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xyz/main.dart';
 
 void main() {
   testWidgets('app boots', (tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ProviderScope(child: MyApp()));
+
     await tester.pump();
     expect(true, isTrue);
   });
