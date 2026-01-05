4854b410f0cf87a54de36f5ebf0e3b29734a3553
OliverHaug <oliver-haugk@web.de>
2026-01-05 01:17:44 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
+  if (!kIsWeb) {
     try {
       await dotenv.load(fileName: '.env');
     } catch (_) {}
